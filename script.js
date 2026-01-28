e640fa89aa7fdb7f623add123344f19a31f28185
volvimen <251765543+volvimem@users.noreply.github.com>
2026-01-28 12:03:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -428,21 +428,11 @@ function abrirPopUpLive() {
 }
 
 function sairModoLive() {
-    // 1. Para os cronômetros para não gastar memória
     if(chronoInterval) clearInterval(chronoInterval);
     if(liveInterval) clearInterval(liveInterval);
-
-    // 2. Esconde a tela de Live
     document.getElementById('screen-live-monitor').style.display = 'none';
-    
-    // 3. CORREÇÃO: Mostra a tela principal do App de volta
-    // Antes estava faltando isso ou estava 'block' errado. 
-    // O correto para o app-container é 'flex' pois ele usa flexbox no CSS.
-    document.getElementById('main-app-container').style.display = 'flex'; 
-
-    // 4. Garante que o usuário volte para a aba de ADMIN (onde ele estava)
+    document.getElementById('main-app-container').style.display = 'block';
     renderContent('adm');
-};
 }
 
 function iniciarLiveLoop() { 
